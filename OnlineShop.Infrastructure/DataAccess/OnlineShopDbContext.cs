33e048090b4f38f3ee5b4ae605feed475547312a
MashaLupiak <maashe4ka@gmail.com>
2024-04-01 17:49:06 +0300

    feat: Added Product model and DbContext using scaffolding

---
+++ OnlineShop.Infrastructure/DataAccess/OnlineShopDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Infrastructure.DataAccess;
+
+public partial class OnlineShopDbContext : DbContext
+{
+    public OnlineShopDbContext()
+    {
+    }
+
+    public OnlineShopDbContext(DbContextOptions<OnlineShopDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=OnlineShopDB;Trusted_Connection=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6CDC3BEED9F");
+
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
